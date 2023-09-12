5ec8f23f4e66663f587c7751f62d734ecf3b7217
Ponthawan <s6506022610031@email.kmutnb.ac.th>
2023-09-12 10:44:46 +0700

    Update

--- CRUDBookSQLite.js
+++ CRUDBookSQLite.js
@@ -78,3 +78,16 @@ app.delete('/books/:id' , (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+///require("dotenv").config();
+//const express = require("express");
+//const app = express();
+//const port = process.env.PORT||5000;
+
+//app.get("/", (req, res) => {
+  //res.send("Hello World!");
+//});
+
+//app.listen(port, () => {
+  //console.log(`Example app listening at http://localhost:${port}`);
+//});
